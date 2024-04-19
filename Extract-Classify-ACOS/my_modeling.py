0337c8d696441fc4174fa0bce325dd2e5c81655f
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-04-19 15:19:52 +0700

    debug

--- Extract-Classify-ACOS/my_modeling.py
+++ Extract-Classify-ACOS/my_modeling.py
@@ -2,6 +2,17 @@ from transformers import PreTrainedModel, AutoModel, AutoConfig, AutoTokenizer
 from torch import nn
 from torchcrf import CRF
 
+class DenseLayer(nn.Module):
+    def __init__(self, config):
+        super(DenseLayer, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        pooled_output = self.dense(hidden_states)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+
 class BertForQuadABSA(nn.Module):
 
     def __init__(self, model_name, config, num_labels=2):
